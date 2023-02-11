b95175d6aaaeaa676a6799f4ffddc4bec1d9657a
Seran Shanmugathas <seran@live.no>
2023-02-11 12:54:16 +0100

    styles: add keyframes in tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,46 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			animations: {
+				'slide-in': {
+					from: {
+						opacity: 0
+					},
+					to: {
+						opacity: 1
+					}
+				},
+				'open-modal': {
+					from: {
+						opacity: 0,
+						transform: 'scale(0)'
+					},
+					to: {
+						opacity: 1,
+						transform: 'scale(1)'
+					}
+				},
+				'slide-out': {
+					from: {
+						opacity: 1
+					},
+					to: {
+						opacity: 0
+					}
+				},
+				'close-modal': {
+					from: {
+						opacity: 1,
+						transform: 'scale(1)'
+					},
+					to: {
+						opacity: 0,
+						transform: 'scale(0)'
+					}
+				}
+			}
+		}
 	},
 	plugins: []
 };
