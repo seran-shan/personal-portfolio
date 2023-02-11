43cafc226833b0af4fba8ad3422edbdfd6ec9d7a
Seran Shanmugathas <seran@live.no>
2023-02-11 15:01:20 +0100

    feat: create project page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,54 +1,289 @@
-<script>
-	import Header from './Header.svelte';
-	import './styles.css';
-	import '../app.css';
+<script lang="ts">
+	import Navbar from '$lib/components/NavBar.svelte';
+	import Button from '$lib/components/Button.svelte';
+	import FaCopy from 'svelte-icons/fa/FaCopy.svelte';
+	import Tooltip from '$lib/components/Tooltip.svelte';
+	import CopyClipBoard from '$lib/components/CopyToClipBoard.svelte';
+	import { beforeNavigate } from '$app/navigation';
+
+	import { page } from '$app/stores';
+	import Modal from '$lib/components/Modal.svelte';
+	import { onMount } from 'svelte';
+	import { customBackground } from '$lib/store';
+	import routes from '$lib/NavRoutes';
+
+	let copied = false;
+	let email = 'cavallogianmarco@gmail.com';
+	const cookieEnabled = false;
+	$: showCookieModal = false;
+
+	const cssVariables = (node: HTMLElement, variables: Record<string, string>) => {
+		setCssVariables(node, variables);
+
+		return {
+			update(variables: Record<string, string>) {
+				setCssVariables(node, variables);
+			}
+		};
+	};
+
+	const setCssVariables = (node: HTMLElement, variables: Record<string, string>) => {
+		for (const name in variables) {
+			node.style.setProperty(`--${name}`, variables[name]);
+		}
+	};
+
+	const copy = () => {
+		const app = new CopyClipBoard({
+			// @ts-ignore
+			target: document.getElementById('clipboard'),
+			props: { email }
+		});
+		app.$destroy();
+	};
+
+	onMount(() => {
+		const showCookie = localStorage.getItem('showCookieModal');
+		if (showCookie !== null) showCookieModal = JSON.parse(showCookie);
+		else showCookieModal = true;
+	});
 </script>
 
-<div class="app">
-	<Header />
+<svelte:body use:cssVariables={{ background: $customBackground }} />
 
-	<main>
-		<slot />
-	</main>
+{#if showCookieModal && cookieEnabled}
+	<div class="cookieContainer">
+		<p>🍪 This website use <a href="privacy-policy">Cookies.</a></p>
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<div
+			on:click={() => {
+				showCookieModal = false;
+				localStorage.setItem('showCookieModal', 'false');
+			}}
+		>
+			&#10005;
+		</div>
+	</div>
+{/if}
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
-</div>
+<Modal>
+	<div slot="content" class="modalContainer">
+		<h1>Email:</h1>
+		<div>
+			<p>{email}</p>
+			&nbsp;
+			<div class="tooltip">
+				<Tooltip tooltip={copied}>
+					<!-- svelte-ignore a11y-click-events-have-key-events -->
+					<div
+						id="clipboard"
+						on:click={() => {
+							copied = true;
+							copy();
+							setTimeout(() => {
+								copied = false;
+							}, 500);
+						}}
+					>
+						<div>
+							<FaCopy />
+						</div>
+					</div>
+				</Tooltip>
+			</div>
+		</div>
+		<Button>Send Email</Button>
+	</div>
+</Modal>
+<Navbar segment={$page.url.pathname} />
+
+<slot />
+
+<footer>
+	Powered with ❤️ by <a class="me" href="/about">Seran</a> in
+	<span class="svelte">Svelte</span>
+</footer>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
+	* {
 		box-sizing: border-box;
 	}
 
-	footer {
+	@font-face {
+		font-family: 'Fira Code', monospace;
+		font-display: optional;
+		src: url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap');
+	}
+
+	:global(#svelte) {
+		width: 100vw;
+		height: 100%;
+		max-width: 900px;
+
 		display: flex;
 		flex-direction: column;
-		justify-content: center;
+		justify-content: space-between;
+	}
+
+	:global(html),
+	:global(body) {
+		transition: background-color 0.2s ease 0s;
+		position: relative;
+		width: 100%;
+		height: 100%;
+		overflow: auto;
+		font-family: 'Fira Code', monospace;
+		background-color: #0a0908;
+	}
+
+	:global(body) {
+		background-color: var(--background);
+		background-size: 200% 200%;
+		color: white;
+		margin: 0;
+		box-sizing: border-box;
+		display: grid;
+		line-height: 1.75;
+		place-items: center;
+		height: 100%;
+		overflow-x: hidden;
+	}
+
+	:global(h1) {
+		border: 0;
+	}
+
+	:global(::selection) {
+		color: white;
+		background: #ca3c25;
+	}
+
+	:global(::-webkit-scrollbar) {
+		width: 8px;
+		height: 8px;
+		border-radius: 1px;
+	}
+
+	:global(::-webkit-scrollbar-thumb) {
+		background-color: #fafffd;
+		border-radius: 3px;
+	}
+
+	:global(::-webkit-scrollbar-track) {
+		background-color: transparent;
+		border-radius: 1px;
+	}
+
+	@media (min-width: 900px) {
+		:global(body) {
+			padding: 0 100px;
+		}
+	}
+
+	:global(a) {
+		text-decoration: none;
+	}
+
+	:global(a) {
+		text-decoration: none;
+	}
+
+	a {
+		color: rgb(0, 100, 200);
+		text-decoration: none;
+	}
+
+	a:hover {
+		text-decoration: underline;
+	}
+
+	a:visited {
+		color: rgb(0, 80, 160);
+	}
+
+	.modalContainer div {
+		display: flex;
+		margin-bottom: 20px;
+	}
+
+	.modalContainer p {
+		margin: 0;
+	}
+
+	:global(.tooltip) {
+		visibility: hidden;
+	}
+
+	.cookieContainer {
+		background: white;
+		border-radius: 0px;
+		text-align: center;
+		width: 100%;
+		height: 30px;
+		color: black;
+		padding: 30px;
+		display: flex;
+		justify-content: space-evenly;
 		align-items: center;
-		padding: 12px;
+		position: fixed;
+		bottom: 0px;
+		left: 0;
+		right: 0;
+		margin-left: auto;
+		margin-right: auto;
+	}
+	.cookieContainer > p > a {
+		text-decoration: underline;
+	}
+
+	.cookieContainer > div {
+		cursor: pointer;
+	}
+
+	footer {
+		font-size: 16px;
+		font-weight: 400;
+		padding: 30px 0;
+		max-width: 900px;
+		text-align: center;
+		width: 100%;
 	}
 
 	footer a {
-		font-weight: bold;
+		text-decoration: none;
+		color: #4158d0;
+	}
+	footer .svelte {
+		color: #ff3e00;
 	}
 
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
+	footer .me {
+		color: #ff3e00;
+	}
+
+	@media (min-width: 900px) {
+		:global(.tooltip) {
+			visibility: visible;
+		}
+	}
+	@media (min-width: 600px) {
+		.cookieContainer {
+			background: white;
+			border-radius: 50px;
+			text-align: center;
+			width: 350px;
+			height: 30px;
+			color: black;
+			padding: 0 10px;
+			display: flex;
+			justify-content: space-evenly;
+			align-items: center;
+			position: fixed;
+			bottom: 50px;
+			left: 0;
+			right: 0;
+			margin-left: auto;
+			margin-right: auto;
 		}
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,108 @@
 <script>
-	import FaEnvelope from 'svelte-icons/fa/FaEnvelope.svelte';
-	import FaMedium from 'svelte-icons/fa/FaMedium.svelte';
 	import FaLinkedin from 'svelte-icons/fa/FaLinkedin.svelte';
 	import FaGithub from 'svelte-icons/fa/FaGithub.svelte';
+	import FaEnvelope from 'svelte-icons/fa/FaEnvelope.svelte';
 	import { modalOpened } from '$lib/store';
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>Seran Shanmugathas - Portofolio</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
-
-<main class="flex flex-col items-center justify-center h-full">
-	<h1 class="text-center text-xl font-bold mb-6">
-		Hi!✋ <br /> I'm Seran a Software Developer 💻 based in Norway
-	</h1>
-	<div class="flex items-center justify-between gap-10 w-64">
+<main>
+	<h1>Hi!✋ <br /> I'm Seran Shanmugathas a Software Developer 💻 based in Norway</h1>
+	<div class="icons">
 		<div
 			on:click={() => {
 				modalOpened.set(true);
 			}}
-			class="text-white cursor-pointer hover:text-indigo-400 transition-colors duration-200"
 		>
-			<FaEnvelope />
+			<div class="icon">
+				<FaEnvelope />
+			</div>
 		</div>
+
 		<a
-			href="https://github.com/Ladvace"
+			href="https://github.com/seran-shan"
 			aria-label="GitHub"
 			target="_blank"
 			rel="noopener noreferrer"
-			class="text-white cursor-pointer hover:text-indigo-400 transition-colors duration-200"
 		>
-			<FaGithub />
+			<div class="icon">
+				<FaGithub />
+			</div>
 		</a>
 		<a
-			href="https://www.linkedin.com/in/ladvace/"
+			href="https://www.linkedin.com/in/seran-shanmugathas/"
 			aria-label="Linkedin"
 			target="_blank"
 			rel="noopener noreferrer"
-			class="text-white cursor-pointer hover:text-indigo-400 transition-colors duration-200"
 		>
-			<FaLinkedin />
-		</a>
-		<a
-			href="https://medium.com/@ladvace"
-			aria-label="Medium"
-			target="_blank"
-			rel="noopener noreferrer"
-			class="text-white cursor-pointer hover:text-indigo-400 transition-colors duration-200"
-		>
-			<FaMedium />
+			<div class="icon">
+				<FaLinkedin />
+			</div>
 		</a>
 	</div>
 </main>
+
+<style>
+	a {
+		color: white;
+		text-decoration: none;
+	}
+	main {
+		text-align: center;
+		padding: 0;
+		margin: 0 auto;
+		text-align: center;
+
+		display: flex;
+		flex-direction: column;
+		height: calc(100vh - 80px - 88px);
+		justify-content: center;
+		align-items: center;
+	}
+
+	h1 {
+		font-weight: 700;
+	}
+
+	main > h1 {
+		margin: 50px 10px 0;
+		font-size: 36px;
+	}
+
+	.icons {
+		display: flex !important;
+		justify-content: center !important;
+		align-items: center;
+		gap: 20px;
+		cursor: pointer;
+		font-size: 30px;
+		display: flex;
+		justify-content: space-between;
+		max-width: 200px;
+		margin: 50px auto 0;
+	}
+
+	.icon {
+		cursor: pointer;
+		transition: color 0.2s ease-in-out;
+		width: 40px;
+	}
+	.icon:hover {
+		color: #ca3c25;
+	}
+
+	@media (min-width: 900px) {
+		main > h1 {
+			font-size: 48px;
+		}
+	}
+
+	@media (min-width: 600px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
